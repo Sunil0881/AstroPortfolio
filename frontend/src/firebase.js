55eceec85abf16aba54868ab44b2010ae218fc98
Deepakraja03 <12306deepakraja@gmail.com>
2024-08-13 18:25:43 +0530

    firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBRsURz3xJ2QDf7MlmLuG3l6ChIZrXZTeQ",
-  authDomain: "astro-d7240.firebaseapp.com",
-  projectId: "astro-d7240",
-  storageBucket: "astro-d7240.appspot.com",
-  messagingSenderId: "677006972917",
-  appId: "1:677006972917:web:d82f68ecbce85154d632bc",
-  measurementId: "G-QY0DFMQCY4"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDINDID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID
 };
 
 // Initialize Firebase
